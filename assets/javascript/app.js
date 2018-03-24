356cdba286f82be25150e0aea4192ab338e41616
Sutton Ashby <korim_fidelis@yahoo.com>
2018-03-24 13:12:27 -0400

    commit before pulling master

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -57,26 +57,8 @@ $(document).ready(function () {
     var categorie = "9";
     var difficulty = "hard";
 
-    var getQuestions = function () {
+    var getQuestions = function () { }
 
-    // example question
-
-    /* {
-        "response_code": 0,
-        "results": [
-        {
-        "category": "General%20Knowledge",
-        "type": "multiple",
-        "difficulty": "medium",
-        "question": "This%20field%20is%20sometimes%20known%20as%20%E2%80%9CThe%20Dismal%20Science.%E2%80%9D",
-        "correct_answer": "Economics",
-        "incorrect_answers": [
-        "Philosophy",
-        "Politics",
-        "Physics"
-        ]
-        }, */
-    // question topics
     var questionCategories = [
         "General Knowledge",
         "Entertainment: Books",
@@ -143,98 +125,85 @@ $(document).ready(function () {
 
     // Calling weather API, getting current conditions in city user is going to, and changing TOTALTIME according to degree of weather
 
-$(".correct").on("click", function () {
-    var city = $("CITY NAME").val() + ".json";
-    console.log(city);
+    $(".correct").on("click", function () {
+        var city = $("CITY NAME").val() + ".json";
+        console.log(city);
 
-    var queryURL = "http://api.wunderground.com/api/c2f13b0c2d6e1c55/conditions/q/" + city;
+        var queryURL = "http://api.wunderground.com/api/c2f13b0c2d6e1c55/conditions/q/" + city;
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        var results = response.current_observation;
-        var weather = results.weather;
-        console.log(weather);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            var results = response.current_observation;
+            var weather = results.weather;
+            console.log(weather);
 
-        // Changing TOTALTIME based on current weather condition of user travel location
-        if ((weather === "Clear") || (weather === "Partly Cloudy") || (weather === "Scattered Clouds")) {
-            TOTALTIME = TOTALTIME - 2; // Take 2 hours off total time for good weather travel
-        } else if ((weather === "Squalls") || (weather === "Small Hail") || (weather === "Funnel Cloud")) {
-            // Go to random location for really bad weather
-        } else {
-            TOTALTIME = TOTALTIME + 2; // Add 2 hours to total time for bad weather travel
-        }
+            // Changing TOTALTIME based on current weather condition of user travel location
+            if ((weather === "Clear") || (weather === "Partly Cloudy") || (weather === "Scattered Clouds")) {
+                TOTALTIME = TOTALTIME - 2; // Take 2 hours off total time for good weather travel
+            } else if ((weather === "Squalls") || (weather === "Small Hail") || (weather === "Funnel Cloud")) {
+                // Go to random location for really bad weather
+            } else {
+                TOTALTIME = TOTALTIME + 2; // Add 2 hours to total time for bad weather travel
+            }
+        })
     })
-})
 
-//returns a random location from the array to be sent to due to bad weather
-var goRand = function () {
-    return randLocs[Math.floor(Math.random() * randLocs.length)]
-}
+    //returns a random location from the array to be sent to due to bad weather
+    var goRand = function () {
+        return randLocs[Math.floor(Math.random() * randLocs.length)]
+    }
 
-//when reaching final destination
-var endgame = function () {
-    playerToLeaderboard() //push player info to leaderboard
-    getLeaderboard() //get leaderboard for display
-    //restart game button
+    //when reaching final destination
+    var endgame = function () {
+        playerToLeaderboard() //push player info to leaderboard
+        getLeaderboard() //get leaderboard for display
+        //restart game button
 
-}
+    }
 
-var config = {
-    apiKey: "AIzaSyDhuFW_sSUhJhs9WifwBaQK1RpzFdG04uI",
-    databaseURL: "https://pbc-groupproject1.firebaseio.com/"
-};
+    var config = {
+        apiKey: "AIzaSyDhuFW_sSUhJhs9WifwBaQK1RpzFdG04uI",
+        databaseURL: "https://pbc-groupproject1.firebaseio.com/"
+    };
 
     firebase.initializeApp(config);
 
     var database = firebase.database();
 
-//on click to submit player name
-var playerChoices = function () {
-    if (userName === undefined) {
-        userName = $(nameInput).val().trim();
-        userName = JSON.stringify(userName);
+    //on click to submit player name
+    var playerChoices = function () {
+        if (userName === undefined) {
+            userName = $(nameInput).val().trim();
+            userName = JSON.stringify(userName);
+        }
+        categoryChoice = $(categoryInput)
     }
-    categoryChoice = $(categoryInput)
-}
-$("#submit").on("click", playerChoices)
+    $("#submit").on("click", playerChoices)
 
-//add to firebase leaderboard
-var playerToLeaderboard = function () {
-    // time it took to finish
-    database.ref().child(userName).set({
-        Name: userName,
-        Score: userScore,
-        Category: categoryChoice
-    })
-}
-
-//pull leaderboard for display at the end of game
-var getLeaderboard = function () {
-
-    //add to leaderboard
-    var userName;//submit form name
-    var userScore; // time it took to finish
-    var categoryChoice; //what category did they play
-    database.ref().child(userName).set({
-        Name: userName,
-        Score: userScore,
-        Category: categoryChoice
-    })
-
-
-    //pull leaderboard
-
-    database.orderByChild("Score").limitToFirst(10).once("value", function (snapshot) {
-        snapshot.forEach(function (child) {
-            var name = child.val().Name;
-            var score = child.val().Score;
-            var category = child.val().Category;
-            $("#leaderboard").append("<tr><td>" + name + "</td><td>" + score + "</td><td>" + category + "</td><tr>");
+    //add to firebase leaderboard
+    var playerToLeaderboard = function () {
+        // time it took to finish
+        database.ref().child(userName).set({
+            Name: userName,
+            Score: userScore,
+            Category: categoryChoice
         })
-    })
+    }
+
+    //pull leaderboard for display at the end of game
+    var getLeaderboard = function () {
+        database.orderByChild("Score").limitToFirst(10).once("value", function (snapshot) {
+            snapshot.forEach(function (child) {
+                var name = child.val().Name;
+                var score = child.val().Score;
+                var category = child.val().Category;
+                $("#leaderboard").append("<tr><td>" + name + "</td><td>" + score + "</td><td>" + category + "</td><tr>");
+            })
+        })
+    }
 
     $('.modal').modal({
         dismissible: true, // Modal can be dismissed by clicking outside of the modal
@@ -251,177 +220,177 @@ var getLeaderboard = function () {
     }
     );
 
-  var countries = {
-    usa: {
-        city: "New York City",
-        country: "United States of America",
-        questionType: "easy",
-        easyLoc: "canada",
-        easyTime: 4,
-        hardLoc: "mexico",
-        hardTime: 8
-    },
-    mexico: {
-        city: "Mexico City",
-        country: "Mexico",
-        questionType: "hard",
-        easyLoc: "venezuela",
-        easyTime: 4,
-        hardLoc: "argentina",
-        hardTime: 8
-    },
-    canada: {
-        city: "Montreal",
-        country: "Canada",
-        questionType: "easy",
-        easyLoc: "venezuela",
-        easyTime: 4,
-        hardLoc: "norway",
-        hardTime: 8
-    },
-    argentina: {
-        city: "Buenos Aires",
-        country: "Argentina",
-        questionType: "hard",
-        easyLoc: "morocco",
-        easyTime: 4,
-        hardLoc: "southAfrica",
-        hardTime: 8
-    },
-    venezuela: {
-        city: "Caracas",
-        country: "Venezuela",
-        questionType: "easy",
-        easyLoc: "morocco",
-        easyTime: 4,
-        hardLoc: "southAfrica",
-        hardTime: 8
-    },
-    norway: {
-        city: "Oslo",
-        country: "Norway",
-        questionType: "hard",
-        easyLoc: "morocco",
-        easyTime: 4,
-        hardLoc: "southAfrica",
-        hardTime: 8
-    },
-    southAfrica: {
-        city: "Cape Town",
-        country: "South Africa",
-        questionType: "hard",
-        easyLoc: "yemen",
-        easyTime: 4,
-        hardLoc: "djibouti",
-        hardTime: 8
-    },
-    morocco: {
-        city: "Casablanca",
-        country: "Morocco",
-        questionType: "easy",
-        easyLoc: "yemen",
-        easyTime: 4,
-        hardLoc: "malta",
-        hardTime: 8
-    },
-    djibouti: {
-        city: "Djibouti",
-        country: "Djibouti",
-        questionType: "hard",
-        easyLoc: "turkmenistan",
-        easyTime: 4,
-        hardLoc: "india",
-        hardTime: 8
-    },
-    yemen: {
-        city: "Sana'a",
-        country: "Yemen",
-        questionType: "easy",
-        easyLoc: "turkmenistan",
-        easyTime: 4,
-        hardLoc: "india",
-        hardTime: 8
-    },
-    malta: {
-        city: "Valletta",
-        country: "Malta",
-        questionType: "hard",
-        easyLoc: "turkmenistan",
-        easyTime: 4,
-        hardLoc: "india",
-        hardTime: 8
-    },
-    india: {
-        city: "New Delhi",
-        country: "India",
-        questionType: "hard",
-        easyLoc: "china",
-        easyTime: 4,
-        hardLoc: "indonesia",
-        hardTime: 8
-    },
-    turkmenistan: {
-        city: "Ashgabat",
-        country: "Turkmenistan",
-        questionType: "easy",
-        easyLoc: "china",
-        easyTime: 4,
-        hardLoc: "Russia",
-        hardTime: 8
-    },
-    indonesia: {
-        city: "Jakarta",
-        country: "Indonesia",
-        questionType: "hard",
-        easyLoc: "japan",
-        easyTime: 4,
-        hardLoc: "papuaNewGuinea",
-        hardTime: 8
-    },
-    china: {
-        city: "Beijing",
-        country: "China",
-        questionType: "easy",
-        easyLoc: "japan",
-        easyTime: 4,
-        hardLoc: "papuaNewGuinea",
-        hardTime: 8
-    },
-    russia: {
-        city: "Moscow",
-        country: "Russia",
-        questionType: "hard",
-        easyLoc: "japan",
-        easyTime: 4,
-        hardLoc: "papuaNewGuinea",
-        hardTime: 8
-    },
-    papuaNewGuinea: {
-        city: "Port Moresby",
-        country: "Papua New Guinea",
-        questionType: "hard",
-        easyLoc: "australia",
-        easyTime: 4,
-        hardLoc: "australia",
-        hardTime: 8
-    },
-    japan: {
-        city: "Tokyo",
-        country: "Japan",
-        questionType: "easy",
-        easyLoc: "australia",
-        easyTime: 4,
-        hardLoc: "australia",
-        hardTime: 8
-    },
-    australia: {
-        city: "Canberra",
-        country: "Australia",
-        questionType: "easy",
-        easyLoc: "australia",
-        easyTime: 4,
-        hardLoc: "australia",
-        hardTime: 8
-    }
-};
+    var countries = {
+        usa: {
+            city: "New York City",
+            country: "United States of America",
+            questionType: "easy",
+            easyLoc: "canada",
+            easyTime: 4,
+            hardLoc: "mexico",
+            hardTime: 8
+        },
+        mexico: {
+            city: "Mexico City",
+            country: "Mexico",
+            questionType: "hard",
+            easyLoc: "venezuela",
+            easyTime: 4,
+            hardLoc: "argentina",
+            hardTime: 8
+        },
+        canada: {
+            city: "Montreal",
+            country: "Canada",
+            questionType: "easy",
+            easyLoc: "venezuela",
+            easyTime: 4,
+            hardLoc: "norway",
+            hardTime: 8
+        },
+        argentina: {
+            city: "Buenos Aires",
+            country: "Argentina",
+            questionType: "hard",
+            easyLoc: "morocco",
+            easyTime: 4,
+            hardLoc: "southAfrica",
+            hardTime: 8
+        },
+        venezuela: {
+            city: "Caracas",
+            country: "Venezuela",
+            questionType: "easy",
+            easyLoc: "morocco",
+            easyTime: 4,
+            hardLoc: "southAfrica",
+            hardTime: 8
+        },
+        norway: {
+            city: "Oslo",
+            country: "Norway",
+            questionType: "hard",
+            easyLoc: "morocco",
+            easyTime: 4,
+            hardLoc: "southAfrica",
+            hardTime: 8
+        },
+        southAfrica: {
+            city: "Cape Town",
+            country: "South Africa",
+            questionType: "hard",
+            easyLoc: "yemen",
+            easyTime: 4,
+            hardLoc: "djibouti",
+            hardTime: 8
+        },
+        morocco: {
+            city: "Casablanca",
+            country: "Morocco",
+            questionType: "easy",
+            easyLoc: "yemen",
+            easyTime: 4,
+            hardLoc: "malta",
+            hardTime: 8
+        },
+        djibouti: {
+            city: "Djibouti",
+            country: "Djibouti",
+            questionType: "hard",
+            easyLoc: "turkmenistan",
+            easyTime: 4,
+            hardLoc: "india",
+            hardTime: 8
+        },
+        yemen: {
+            city: "Sana'a",
+            country: "Yemen",
+            questionType: "easy",
+            easyLoc: "turkmenistan",
+            easyTime: 4,
+            hardLoc: "india",
+            hardTime: 8
+        },
+        malta: {
+            city: "Valletta",
+            country: "Malta",
+            questionType: "hard",
+            easyLoc: "turkmenistan",
+            easyTime: 4,
+            hardLoc: "india",
+            hardTime: 8
+        },
+        india: {
+            city: "New Delhi",
+            country: "India",
+            questionType: "hard",
+            easyLoc: "china",
+            easyTime: 4,
+            hardLoc: "indonesia",
+            hardTime: 8
+        },
+        turkmenistan: {
+            city: "Ashgabat",
+            country: "Turkmenistan",
+            questionType: "easy",
+            easyLoc: "china",
+            easyTime: 4,
+            hardLoc: "Russia",
+            hardTime: 8
+        },
+        indonesia: {
+            city: "Jakarta",
+            country: "Indonesia",
+            questionType: "hard",
+            easyLoc: "japan",
+            easyTime: 4,
+            hardLoc: "papuaNewGuinea",
+            hardTime: 8
+        },
+        china: {
+            city: "Beijing",
+            country: "China",
+            questionType: "easy",
+            easyLoc: "japan",
+            easyTime: 4,
+            hardLoc: "papuaNewGuinea",
+            hardTime: 8
+        },
+        russia: {
+            city: "Moscow",
+            country: "Russia",
+            questionType: "hard",
+            easyLoc: "japan",
+            easyTime: 4,
+            hardLoc: "papuaNewGuinea",
+            hardTime: 8
+        },
+        papuaNewGuinea: {
+            city: "Port Moresby",
+            country: "Papua New Guinea",
+            questionType: "hard",
+            easyLoc: "australia",
+            easyTime: 4,
+            hardLoc: "australia",
+            hardTime: 8
+        },
+        japan: {
+            city: "Tokyo",
+            country: "Japan",
+            questionType: "easy",
+            easyLoc: "australia",
+            easyTime: 4,
+            hardLoc: "australia",
+            hardTime: 8
+        },
+        australia: {
+            city: "Canberra",
+            country: "Australia",
+            questionType: "easy",
+            easyLoc: "australia",
+            easyTime: 4,
+            hardLoc: "australia",
+            hardTime: 8
+        }
+    };
 });
