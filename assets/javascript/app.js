9d5acf83a2f89957e4beeda1849c1d5a1c1ac375
Sutton Ashby <korim_fidelis@yahoo.com>
2018-03-29 15:42:06 -0400

    modal leaves on submit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -475,6 +475,7 @@ $(document).ready(function () {
         }
         $("#beginButton").css("visibility", "hidden")
         categoryChoice = $("#dropdown option:selected").val()
+        $("#modal1").removeClass("open") //ideally this should be toggled instead
     }
 
     $("#submit").on("click", playerChoices)
