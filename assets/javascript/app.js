1c8ae0201edeb2696c6db37e758508373811899e
Sutton Ashby <korim_fidelis@yahoo.com>
2018-03-29 10:55:36 -0400

    hid the cards and moved the clock start until after submitting player name

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -424,13 +424,40 @@ $(document).ready(function () {
     };
     firebase.initializeApp(config);
     var database = firebase.database()
+
+    var countdown;
+
     var playerChoices = function () { //on click to submit player name
         if (userName === undefined) {
             userName = $("#icon_prefix2").val().trim();
             userName = JSON.stringify(userName);
+            $("#card1").css("visibility", "visible")
+            $("#card2").css("visibility", "visible")
             cardEasyDestination();
             cardHardDestination();
             getWeather();
+            $(function () {
+                FlipClock.Lang.Custom = { days: 'Days', hours: 'Hours', minutes: 'Minutes', seconds: 'Seconds' };
+                var opts = {
+                    clockFace: 'DailyCounter',
+                    countdown: false,
+                    language: 'Custom'
+                };
+                opts.classes = {
+                    active: 'flip-clock-active',
+                    before: 'flip-clock-before',
+                    divider: 'flip-clock-divider',
+                    dot: 'flip-clock-dot',
+                    label: 'flip-clock-label',
+                    flip: 'flip',
+                    play: 'play',
+                    wrapper: 'flip-clock-small-wrapper'
+                };
+                var countdown = 0;
+                //1521851280 - ((new Date().getTime())/1000); // from: 03/23/2018 08:28 pm -0400
+                //countdown = Math.max(1, countdown);
+                $('.clock-builder-output').FlipClock(countdown, opts);
+            });
         }
         categoryChoice = $("#dropdown option:selected").val()
     }
